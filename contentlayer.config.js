f46fed199a432f0a637901b257a4ac82a956cce4
Saply <88439700+Saply@users.noreply.github.com>
2024-09-25 21:47:01 +0800

    Add render MDX and blog detail components

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,9 +1,14 @@
 import { makeSource, defineDocumentType } from "contentlayer2/source-files";
+import readingTime from "reading-time";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 // https://contentlayer.dev/docs/reference/source-files/define-document-type-eb9db60e
 const Blog = defineDocumentType(() => ({
   name: "Blog",
   filePathPattern: "**/**/*.mdx",
+  contentType: "mdx",
   fields: {
     title: {
       type: "string",
@@ -43,10 +48,15 @@ const Blog = defineDocumentType(() => ({
       type: "string",
       resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
     },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw)
+    }
   },
 }));
 
 export default makeSource({
   contentDirPath: "content",
   documentTypes: [Blog],
+  mdx: { remarkPlugins: [remarkGfm], rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {behavior: "append"}]]}
 });
