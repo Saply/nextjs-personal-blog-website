9c132a5096c5adcee099744e6b2ac517b9e8f448
Saply <88439700+Saply@users.noreply.github.com>
2024-10-08 16:34:28 +0800

    Change link icon for headers

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,8 +1,21 @@
 import { makeSource, defineDocumentType } from "contentlayer2/source-files";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
 import readingTime from "reading-time";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
+
+const svgData = fromHtmlIsomorphic(
+  `
+  <span class="content-header-link">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#ec3d60" className="w-5 h-5 linkicon">
+  <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
+  <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
+  </svg>
+  </span>
+`,
+  { fragment: true }
+);
 
 // https://contentlayer.dev/docs/reference/source-files/define-document-type-eb9db60e
 const Blog = defineDocumentType(() => ({
@@ -28,7 +41,6 @@ const Blog = defineDocumentType(() => ({
     },
     coverImage: {
       type: "image",
-      
     },
     isPublished: {
       type: "boolean",
@@ -50,13 +62,28 @@ const Blog = defineDocumentType(() => ({
     },
     readingTime: {
       type: "json",
-      resolve: (doc) => readingTime(doc.body.raw)
-    }
+      resolve: (doc) => readingTime(doc.body.raw),
+    },
   },
 }));
 
 export default makeSource({
   contentDirPath: "content",
   documentTypes: [Blog],
-  mdx: { remarkPlugins: [remarkGfm], rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {behavior: "append"}]]}
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          headingProperties: {
+            className: ["content-header"],
+          },
+          content: svgData,
+        },
+      ],
+    ],
+  },
 });
