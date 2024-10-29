734bcddb33782d5c02126ef569399a857dd26fa2
Saply <88439700+Saply@users.noreply.github.com>
2024-10-29 16:52:17 +0800

    Add RSS feed

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -1,7 +1,42 @@
+import { allBlogs } from "@/.contentlayer/generated";
+import { sortBlogs } from "@/src/utils";
+import siteMetadata from "@/src/utils/siteMetadata";
+
+const generateRssItem = (blog) => `
+  <item>
+    <guid>${siteMetadata.siteUrl}/blogs/${blog._raw.flattenedPath}</guid>
+    <title>${blog.title}</title>
+    <link>${siteMetadata.siteUrl}/blogs/${blog._raw.flattenedPath}</link>
+    ${blog.description && `<description>${blog.description}</description>`}
+    <pubDate>${new Date(blog.publishedAt).toUTCString()}</pubDate>
+    <media:thumbnail width="240" height="135" url="${siteMetadata.siteUrl}${blog.coverImage.filePath.replace("../public", "")}"/>
+    <author>${siteMetadata.email} (${siteMetadata.author})</author>
+    ${blog.tags && blog.tags.map((t) => `<category>${t}</category>`).join("")}
+
+  </item>
+`;
+//  xmlns:media="http://search.yahoo.com/mrss/
 export async function GET() {
-  return new Response("<Feed>", {
+  const sortedBlogs = sortBlogs(allBlogs);
+
+  const feed = `<?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
+      <channel>
+        <title>${siteMetadata.title}</title>
+        <link>${siteMetadata.siteUrl}/categories/all</link>
+        <description>${siteMetadata.description}</description>
+        <language>${siteMetadata.language}</language>
+        <lastBuildDate>${new Date(sortedBlogs[0].publishedAt).toUTCString()}</lastBuildDate>
+        <atom:link rel="self" href="${siteMetadata.siteUrl}/feed.xml" type="application/rss+xml"/>
+          ${sortedBlogs.map((blog) => generateRssItem(blog)).join("")}
+      </channel>
+    </rss>
+    `;
+
+  return new Response(feed, {
+    status: 200,
     headers: {
-      "Content-Type": "application/atom+xml; charset=utf-8",
+      "Content-Type": "application/rss+xml; charset=utf-8",
     },
   });
 }
